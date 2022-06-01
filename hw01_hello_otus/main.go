f548275039b47b8e13f9b7a64e9a27e25491bce4
Avilov <diavilov@sbercloud.ru>
2022-06-02 00:12:12 +0300

    Revert "first homework"
    This reverts commit 2d37893000087a27eae41a1811e3f2993c28ed86.

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"golang.org/x/example/stringutil"
-)
-
 func main() {
-	const entry = "Hello, OTUS!"
-
-	fmt.Println(stringutil.Reverse(entry))
+	// Place your code here.
 }
