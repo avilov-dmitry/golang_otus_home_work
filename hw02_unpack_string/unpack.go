674235e7b2a92bbb150af5bd3eb7209a9750fe13
Avilov <diavilov@sbercloud.ru>
2022-06-10 02:14:11 +0300

    hw02_unpack_string: Unpack

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,40 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var unpacked strings.Builder
+
+	if input == "" {
+		return "", nil
+	}
+
+	fmt.Println("input", input)
+
+	for index, character := range input {
+		if index == 0 {
+			unpacked.WriteString(string(character))
+		} else {
+			digit, _ := strconv.Atoi(string(character))
+			previous := string(input[index-1])
+
+			if unicode.IsDigit(character) && digit > 0 {
+				letters := strings.Repeat(previous, digit-1)
+				unpacked.WriteString(letters)
+			} else {
+				unpacked.WriteString(string(character))
+			}
+		}
+		fmt.Println(string(character), unpacked.String())
+
+	}
+
+	return unpacked.String(), nil
 }
