cba552390795f40cfd57a46a078c5866d6afcd22
Avilov <diavilov@sbercloud.ru>
2022-08-13 17:11:18 +0300

    hw05_parallel_execution: add gorutins and chan

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,25 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	taskChan := make(chan Task)
+	wg := sync.WaitGroup{}
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for task := range taskChan {
+				task()
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+		taskChan <- task
+	}
+
+	close(taskChan)
+
+	wg.Wait()
 	return nil
 }
